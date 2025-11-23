afb1defdfb098a12981be2c0a091ab144401c773
Kristopher Micinski <krismicinski@gmail.com>
2025-11-23 01:08:56 -0500

    removing old files

--- p4/fails-typechecking-programs/fail-r2-3.scm
+++
@@ -1,7 +0,0 @@
-(program
-  (let ([a (read)])
-    (let ([b (+ a 3)])
-      (let ([c (+ (- b) (+ a 7))])
-        (if (not c)               
-            (+ c 1)
-            (- c))))))
--- p4/fails-typechecking-programs/fail-r2-5.scm
+++
@@ -1,5 +0,0 @@
-(program
-  (let ([x (read)])
-    (let ([x (eq? x 0)])               
-      (let ([y (if x 10 20)])
-        (+ y (+ 1 x))))))
